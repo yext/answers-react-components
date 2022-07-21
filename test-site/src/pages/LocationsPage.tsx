56885ff2ee66131ed94279e6428ea533be423299
Alex Taing <ataing@yext.com>
2022-07-21 09:15:49 -0400

    remove excess comments

--- test-site/src/pages/LocationsPage.tsx
+++ test-site/src/pages/LocationsPage.tsx
@@ -23,7 +23,7 @@ export function LocationsPage() {
     zoom: 10,
     style: 'mapbox://styles/mapbox/streets-v11',
   }
-  
+
   return (
       <div>
         <SearchBar />
@@ -49,51 +49,3 @@ export function LocationsPage() {
       </div>
   );
 }
-
-// export function LocationsPage() {
-//   const answersActions = useAnswersActions();
-//   useLayoutEffect(() => {
-//     answersActions.setVertical('KM');
-//   });
-
-//   const customCssClasses: GoogleMapsCssClasses = {
-//     googleMapsContainer: 'h-screen'
-//   }
-
-//   return (
-//       <div>
-//         <SearchBar />
-//         <div className='flex'>
-//           <div className='min-w-fit pr-4'>
-//           </div>
-//           <div className='flex-grow'>
-//             <div className='flex items-baseline'>
-//               <ResultsCount />
-//               <AppliedFilters />
-//             </div>
-//             <div className='grid grid-cols-2 gap-4'>
-//               <div>
-//               <VerticalResults
-//                 CardComponent={StandardCard}
-//               />
-//               </div>
-//               <div>
-//                 <GoogleMaps 
-//                   apiKey='AIzaSyB5D45ghF1YMfqTLSzWubmlCN1euBVPhFw'
-//                   centerLatitude={37.540778} 
-//                   centerLongitude={-77.433928} 
-//                   defaultZoom={10} 
-//                   showEmptyMap={true}
-//                   providerOptions={providerOptions}
-//                   customCssClasses={customCssClasses}
-//                 />
-//               </div>
-//             </div>
-//             <Pagination />
-//             <LocationBias />
-//           </div>
-//         </div>
-//       </div>
-//   );
-// }
-
